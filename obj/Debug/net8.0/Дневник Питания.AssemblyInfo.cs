d430b67b64f2e0a8d6214d95690ffe1767b59f17
Pandora16 <victoria.marengo16@mail.ru>
2024-11-06 23:02:35 +0300

    рабочая версия лабы: требует глубокой декомпозиции и тестов

--- obj/Debug/net8.0/Дневник Питания.AssemblyInfo.cs
+++ obj/Debug/net8.0/Дневник Питания.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Дневник Питания")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+57a5964a225f1af8c0170a488f060cf7f8f7a7dd")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+c859d6586e8fc8767c513fda81e31873423f9f9d")]
 [assembly: System.Reflection.AssemblyProductAttribute("Дневник Питания")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Дневник Питания")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
